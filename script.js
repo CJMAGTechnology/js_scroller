3df111a903bf62fc10b8740fd58cfdec035806f3
CJMAGTechnology <jethro45@hotmail.com>
2022-03-06 08:40:44 -0600

    vid 4

--- script.js
+++ script.js
@@ -12,19 +12,34 @@ const count = 30
 const apiKey = 'ocNG58F-j2oTMDMYHkG2XDP9FO3J5KMadvO6ZWhwp6Q'
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
 
+// helper function
+function setAttributes(element, attributes){
+    for (const key in attributes){
+        element.setAttribute(key, attributes[key])
+    }
+}
 
 // create elements for links and photos add to dom
 function displayPhotos() {
     photosArray.forEach((photo) => {
             // create <a> to link to unsplash
         const item = document.createElement('a')
-        item.setAttribute('href', photo.links.html)
-        item.setAttribute('target', '_blank')
+        // item.setAttribute('href', photo.links.html)
+        // item.setAttribute('target', '_blank')
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank'
+        })
         // create imaget for photo
         const img = document.createElement('img')
-        img.setAttribute('src', photo.urls.regular)
-        img.setAttribute('alt', photo.alt_description)
-        img.setAttribute('tifle', photo.alt_description)
+        // img.setAttribute('src', photo.urls.regular)
+        // img.setAttribute('alt', photo.alt_description)
+        // img.setAttribute('tifle', photo.alt_description)
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt.description,
+            title: photo.alt.description
+        })
 
         //  put <image> inside <a> both instide 
         item.appendChild(img)
