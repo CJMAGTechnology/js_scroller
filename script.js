7abfc2e0577d5fefd54612f9216b3901c1fb10df
CJMAGTechnology <jethro45@hotmail.com>
2022-03-06 09:14:21 -0600

    final

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const imageContainer = document.getElementById('image-container')
 const loader = document.getElementById('loader')
 
 
+let ready = false
+let imagesLoaded = 0
+let totalImages = 0
 let photosArray = []
 
 
@@ -12,6 +15,18 @@ const count = 30
 const apiKey = 'ocNG58F-j2oTMDMYHkG2XDP9FO3J5KMadvO6ZWhwp6Q'
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
 
+// check if images loaded
+function imageLoaded(){
+    imageLoaded ++
+    if (imageLoaded === totalImages){
+        ready = true
+        loader.hidden = true
+
+    }
+
+}
+
+
 // helper function
 function setAttributes(element, attributes){
     for (const key in attributes){
@@ -21,6 +36,10 @@ function setAttributes(element, attributes){
 
 // create elements for links and photos add to dom
 function displayPhotos() {
+    imageLoaded = 0
+
+    totalImages = photosArray.length
+
     photosArray.forEach((photo) => {
             // create <a> to link to unsplash
         const item = document.createElement('a')
@@ -40,6 +59,8 @@ function displayPhotos() {
             alt: photo.alt_description,
             title: photo.alt_description,
         })
+
+        img.addEventListener('load', imageLoaded)
 
         //  put <image> inside <a> both instide 
         item.appendChild(img)
@@ -63,7 +84,8 @@ async function getPhotos() {
 
 // check when scrolling and load more photos
 window.addEventListener('scroll', () => {
-    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000){
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready){
+        ready = false
         getPhotos()
 
     }
